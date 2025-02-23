d0c5daa05c900dc5ff7efc48a7a294a295dc6419
Rana Baranski <rbarans1@asu.edu>
2025-02-23 11:31:00 -0700

    code to reset system / delete all data in tables

---
+++ system/reset_data.py
@@ -0,0 +1,42 @@
+## DO NOT RUN UNTIL WE ARE DONE IMPLEMENTING
+
+# import mysql.connector
+
+# def delete_all_data():
+#     try:
+#         # Establish database connection
+#         conn = mysql.connector.connect(
+#             host='107.180.1.16',
+#             port= 3306,
+#             user='cis440springA2025team3',
+#             password='cis440springA2025team3',
+#             database='cis440springA2025team3'
+#         )
+#         cursor = conn.cursor()
+
+#         # Get all table names
+#         cursor.execute("SHOW TABLES")
+#         tables = cursor.fetchall()
+
+#         # Disable foreign key checks to prevent constraint errors
+#         cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
+
+#         # Delete all data from each table
+#         for (table,) in tables:
+#             cursor.execute(f"DELETE FROM `{table}`")
+#             print(f"Deleted all data from {table}")
+
+#         # Re-enable foreign key checks
+#         cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
+
+#         # Commit changes and close connection
+#         conn.commit()
+#         cursor.close()
+#         conn.close()
+#         print("All tables cleared successfully.")
+
+#     except mysql.connector.Error as err:
+#         print(f"Error: {err}")
+
+# # Run the function
+# delete_all_data()
